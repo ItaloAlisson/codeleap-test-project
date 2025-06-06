338a1e808e3ac7a00448480676f26fec9b8e5871
Italo Alisson <italo.silva@basehub.com.br>
2025-06-06 17:59:49 -0300

    build: Add initial structure

---
+++ src/main/java/uk/co/codeleap/careers/CareersApplication.java
@@ -0,0 +1,13 @@
+package uk.co.codeleap.careers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareersApplication.class, args);
+	}
+
+}
